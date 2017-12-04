b8d37807e43946c3de31a902e4186d44ae0d65c7
Divyam Nandanwar <ndivyam14@gmail.com>
2017-12-04 23:33:51 +0530

    Styling done

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-angular.module('closr',['ngAria','ngAnimate','ngMaterial','ngMessages']);
+angular.module('closr',['ngAria','ngAnimate','ngMaterial','ngMessages'])
+.config(function($mdThemingProvider) {
+       $mdThemingProvider.disableTheming();
+   });
